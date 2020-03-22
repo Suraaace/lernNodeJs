43a98dd1791cc10b1d160c75c4966e39b2d6937b
Kiran Mulmi <kiran.mulmi@monotype.com>
2020-03-22 19:53:28 +0545

    CRUD API integration

--- index.js
+++ index.js
@@ -39,29 +39,162 @@ app.get('/home', function(req, res){
     res.send('You are logged in username:' + username + ' password:' + password);
  });
 
- app.get('/aboutus', function(req, res){
+ app.get('/aboutUs', function(req, res){
     res.render('aboutUs');
  });
 
- app.get('/contactus', function(req, res){
+ app.get('/contactUs', function(req, res){
     res.render('contactUs');
  });
 
+ // CRUD => Ceate
  app.get('/signup', function(req, res){
     res.render('signup');
  });
 
+ // CRUD => Ceate
  app.post('/singUpConfirm', function(req, res){
     let firstname = req.body.first_name;
     let lastname = req.body.last_name;
     let email = req.body.email;
     let phone = req.body.phone;
     let username = firstname+ " " + lastname;
-    
+    // Your Insert to database code begins
+    // MongoDB, MySql, PostGres
+
    res.send('Your registered username, email and phone no is  : ' + username + " " + email + " " + phone );
-//    res.send('Your registered phone no is  : ' + phone);  
-// problem 1:- couldn't send the info in two lines
+    //    res.send('Your registered phone no is  : ' + phone);  
+    // problem 1:- couldn't send the info in two lines
    
+});
+
+
+// CRUD API => Create
+app.post('/user/create', (req, res) => {
+    let myFirstname = req.body.firstName;
+    let myLastname = req.body.lastName;
+    let myEmail = req.body.email;
+
+    let user = {
+        _id: '',
+        firstname: myFirstname,
+        lastname: myLastname,
+        email: myEmail
+    };
+
+    //Database insert code begins
+    var randomstring = require("randomstring");
+    user._id = randomstring.generate();
+
+    let response = {
+        success: true,
+        status: 200,
+        message: "User is successfully created",
+        data:user
+    };
+    res.send(response);
+});
+
+// CRUD API => Read
+app.get('/user', (req, res) => {
+
+    //Fetch Data from Database
+    let sampleDataFromDatabase = [
+        {
+            firstName: "Kiran",
+            lastName: "Mulmi",
+            email: "kiran.mulmi@gmail.com",
+            phoe: "1234567890"
+        },
+        {
+            firstName: "Suresh",
+            lastName: "Shrestha",
+            email: "suresh.shrestha@gmail.com",
+            phoe: "12345340"
+        }
+    ];
+
+    let response = {
+        success: true,
+        message: "List of users",
+        data: sampleDataFromDatabase
+    };
+
+    res.send(response)
+
+});
+
+// CRUD API UPDATE PART-1
+app.get('/user/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    //Fetch Data from Database
+
+    let sampleDataFromDatabase = {
+        _id: "oijkdf8uu324df",
+        firstName: "Suresh",
+        lastName: "Shrestha",
+        email: "suresh.shrestha@gmail.com",
+        phoe: "12345340"
+    }
+
+    let response = {
+        success: true,
+        message: "User details",
+        data: sampleDataFromDatabase
+    };
+
+    res.send(response)
+
+});
+
+
+// CRUD API UPDATE PART-2
+app.post('/user/update/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    let myFirstname = req.body.firstName;
+    let myLastname = req.body.lastName;
+    let myEmail = req.body.email;
+
+    let user = {
+        _id: id,
+        firstname: myFirstname,
+        lastname: myLastname,
+        email: myEmail
+    };
+
+    //Database update code begins
+
+
+    let response = {
+        success: true,
+        status: 200,
+        message: "User is successfully updated",
+        data: user
+    };
+    res.send(response);
+});
+
+// CRUD API => Delete
+app.delete('/user/delete/:id', (req, res) => {
+
+    //Fetch Data from Database
+
+    let id = req.params.id;
+    
+    //Database relate stuff begin
+
+    let response = {
+        success: true,
+        status: 200,
+        message: "User is successfully deleted",
+        data:[]
+    };
+    res.send(response);
+
 });
 
 app.listen(3000);
@@ -78,3 +211,20 @@ console.log('Server is started at http://localhost:3000');
 // about us
 // register page
 // bootstrap design
+
+// Git Branching Flow
+// master
+// develop
+// feature
+// hotFix
+// git branch <Branch Name> => it will create a branch
+// git branch => listing all branch
+// git branch -d <Branch Name> => it will delete the branch
+
+// Method  | filename
+// create => form
+// read => index
+// update => form
+// delete => no file name
+
+//npm install -g nodemon
