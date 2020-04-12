d533cd216d7e48b135544e63faff34f9c248b979
Kiran Mulmi <kiran.mulmi@monotype.com>
2020-04-12 17:33:13 +0545

    product detail page design

--- frontend/src/pages/frontend/modules/ProductDetail.js
+++ frontend/src/pages/frontend/modules/ProductDetail.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {ProductCard} from "./ProductCard";
 
 export const ProductDetail = props => {
     const id = props.match.params.id;
 
     const [stop, setStop] = useState("");
     const [product, setProduct] = useState("");
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         axios.get(process.env.REACT_APP_API_HOST_URL+'/product/'+id)
@@ -13,13 +15,69 @@ export const ProductDetail = props => {
                 setProduct(response.data.data);
             })
             .catch(err => err);
+
+        axios.get(process.env.REACT_APP_API_HOST_URL+'/product/')
+            .then((response) => {
+                setProducts(response.data.data);
+            })
+            .catch(err => err);
     }, [stop]);
 
     return (
-        <div>
-            <div>Name: {product.name} </div>
-            <div>Description: {product.description}</div>
-            <div>Price: {product.price}</div>
+        <div style={{marginTop: 10}}>
+            {/*<div>Name: {product.name} </div>*/}
+            {/*<div>Description: {product.description}</div>*/}
+            {/*<div>Price: {product.price}</div>*/}
+            <div className={'row'}>
+                <div className={'col-5'}>
+                    <div>
+                        <img src={'https://dummyimage.com/600'} className="img-fluid"/>
+                    </div>
+                    <div className={'row'}>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                        <div className={'col-2'}><img src={'https://dummyimage.com/600'} className="img-thumbnail"/></div>
+                    </div>
+                </div>
+                <div className={'col-7'}>
+                    <h2>Product Name</h2>
+                    <p>Magnetic Wireless bluetooth Earphone XT11 music headset Phone Neckband sport Earbuds Earphone with Mic For iPhone Samsung Xiaomi</p>
+                    <div>
+                        <h4><span className="badge badge-secondary">Price: $400</span></h4>
+                    </div>
+                    <div>
+                        <h4>Quantity</h4>
+                        <p>5</p>
+                    </div>
+                    <div>
+                        <h4>Shipping Details</h4>
+                        <p>Magnetic Wireless bluetooth Earphone XT11 music headset Phone Neckband sport Earbuds Earphone with Mic For iPhone Samsung Xiaomi.
+                            Magnetic Wireless bluetooth Earphone XT11 music headset Phone Neckband sport Earbuds Earphone with Mic For iPhone Samsung Xiaomi</p>
+                    </div>
+                    <div className={'row'}>
+                        <div className={'col-4'}>
+                            <button type={'button'} className={'btn btn-primary btn-block'}>Add To cart</button>
+                        </div>
+                        <div className={'col-4'}>
+                            <button type={'button'} className={'btn btn-danger btn-block'}>Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <h2>Related Items</h2>
+            <div className={'row'}>
+                {
+                    products.map((prod, i) => {
+                        return (
+                            <ProductCard product={prod} key={i}/>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };
